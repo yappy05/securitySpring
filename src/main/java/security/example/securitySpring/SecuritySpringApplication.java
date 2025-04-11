2be5de74ca8949499fc395cd4247ac737dd3b2f8
sergey <wendingnn@gmail.com>
2025-04-12 00:56:01 +0300

    init repo

---
+++ src/main/java/security/example/securitySpring/SecuritySpringApplication.java
@@ -0,0 +1,13 @@
+package security.example.securitySpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuritySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security/example/securitySpring/SecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package security.example.securitySpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
