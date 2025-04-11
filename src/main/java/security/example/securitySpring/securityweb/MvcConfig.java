b7797b0e64d32934153e7a3b47ff80b2de6762b2
sergey <wendingnn@gmail.com>
2025-04-12 01:15:39 +0300

    first section. Create an unsecurity application

---
+++ src/main/java/security/example/securitySpring/securityweb/MvcConfig.java
@@ -0,0 +1,14 @@
+package security.example.securitySpring.securityweb;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class MvcConfig implements WebMvcConfigurer {
+    public void addViewConrollers(ViewControllerRegistry registry){
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+}
